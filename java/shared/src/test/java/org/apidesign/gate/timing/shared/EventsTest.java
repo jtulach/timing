5a948b065819ff7d5a1a8838c08eaf950b761fb6
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2017-03-07 04:43:40 +0100

    Always print two digits for minutes and seconds

--- java/shared/src/test/java/org/apidesign/gate/timing/shared/EventsTest.java
+++ java/shared/src/test/java/org/apidesign/gate/timing/shared/EventsTest.java
@@ -7,10 +7,10 @@ public class EventsTest {
     @Test
     public void shortWhenShowsMinutesAndSeconds() {
         Event ev1 = new Event(10, 1488855865759L, "TIME-CHECK");
-        assertEquals("4'25''", ev1.getShortWhen());
+        assertEquals("04'25''", ev1.getShortWhen());
         Event ev2 = new Event(10, 1488855865973L, "TIME-CHECK");
-        assertEquals("4'25''", ev2.getShortWhen());
+        assertEquals("04'25''", ev2.getShortWhen());
         Event ev3 = new Event(10, 1488855866759L, "TIME-CHECK");
-        assertEquals("4'26''", ev3.getShortWhen());
+        assertEquals("04'26''", ev3.getShortWhen());
     }
 }
